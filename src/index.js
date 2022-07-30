a8bff331a227acd1929cb01f387888ba03098136
Kostia Lytvynenko <l.kostiantyn@gmail.com>
2022-07-30 15:59:21 +0300

    Mes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const TodoList = () => {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    const items = ['Drink Coffee', 'Build Awesome App', 'Learn Redux'];
+    return (
+        <ul>
+            <li>{items[0]}</li>
+            <li>{items[1]}</li>
+            <li>{items[2]}</li>
+        </ul>
+    );
+};
+
+const AppHeader = () => {
+    return <h1>Game list</h1>
+};
+
+const SearchPanel = () => {
+    const searchText = 'Type here to search'
+    return <input placeholder={searchText} />
+};
+
+const App = () => {
+
+    return (
+        <div>
+            <AppHeader /> 
+            <SearchPanel />
+            <TodoList />
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));

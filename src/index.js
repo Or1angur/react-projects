c05902a0ee0ebefb81fb10e2cd38aa665003ab69
Kostia Lytvynenko <l.kostiantyn@gmail.com>
2022-08-02 22:57:33 +0300

    structure final

--- src/index.js
+++ src/index.js
@@ -1,33 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-import AppHeader from "./components/app-header/";
-import SearchPanel from "./components/search-panel";
-import TodoList from "./components/todo-list";
-import ItemStatusFilter from "./components/item-status-filter";
-
-import './index.css';
-
-const App = () => {
-
-    const todoData = [
-        { label: 'Drink tea', important: false, id: 1 },
-        { label: 'Eat meat', important: true, id: 2 },
-        { label: 'Sleep a lot', important: false, id: 3 }
-    ];
-
-    return (
-        <div className="todo-app">
-        <AppHeader toDo={1} done={3} />
-        <div className="top-panel d-flex">
-          <SearchPanel />
-          <ItemStatusFilter />
-        </div>
-  
-        <TodoList todos={todoData} />
-      </div>
-    );
-};
+import App from "./components/app";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root')
